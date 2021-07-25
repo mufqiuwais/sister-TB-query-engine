c95f25f94f6c139c10cf6d9bb705d5d34ce17e0d
mufqiuwais <50223787+mufqiuwais@users.noreply.github.com>
2021-07-25 12:43:05 +0700

    3 server + mongo

--- SocketClient/client.py
+++ SocketClient/client.py
@@ -1,9 +1,20 @@
 import socket
 
 ClientSocket = socket.socket()
-host = '127.0.0.1'
+host1 = '127.0.0.1'
+host2 = '127.0.0.2'
+host3 = '127.0.0.3'
 port = 1233
 
+Input = input('Pilih server: ')
+if Input == '1':
+  host = host1
+elif Input == '2':
+  host = host2
+elif Input == '3':
+  host = host3
+
+print(host)
 print('Waiting for connection')
 try:
     ClientSocket.connect((host, port))
